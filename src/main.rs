15868f791bdc21daacb9934e7db7457bc9054f61
computerSmokio <overjazz16@gmail.com>
2022-04-23 01:43:47 -0300

    simple read after end

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+use std::io::Read;
+use std::string::FromUtf8Error;
+
+fn listen_stream(mut stream: TcpStream) -> std::result::Result<String, FromUtf8Error>{
+    let mut buffer = Vec::new();
+    stream.read_to_end(&mut buffer).unwrap();
+    let chain = String::from_utf8(buffer);
+    println!("{:?}", chain);
+    Ok(chain?)
+}
+
+fn main() -> std::io::Result<()>{
+    let listener = TcpListener::bind("127.0.0.1:80").unwrap();
+    for stream in listener.incoming(){
+        let chain = listen_stream(stream?).unwrap();
+        println!("{}", chain);
+    }
+    Ok(())
 }
